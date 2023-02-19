fd63f019053c85e0df59dcc5ec998784a7ef1ea7
nairobiHunter <123130200+nairobiHunter@users.noreply.github.com>
2023-02-19 12:17:56 +0300

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,4 @@
+print('I apologies for not been able to do week 1 Assignment. I found the assignment difficult to do for a begginer like myself')
+print('wrote my first piece of code Hello World on Wednesday')
+print ('that is the only coding I wrote prior to the assignment')
+print ('Next time I will make sure to do or ask for help if I do find it difficult') 
